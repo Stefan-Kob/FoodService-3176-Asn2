92cdeeeb0f37d0eea190a1283d92c1e51737946a
Stefan <103537403+Stefan-Kob@users.noreply.github.com>
2026-02-20 18:43:02 -0500

    Add project files.

---
+++ Controllers/FoodController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace FoodService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FoodController : Controller
+    {
+
+        // DTO to used by this controller
+        public sealed record Food(int Id, string Name, string Description, int Calories);
+
+        // List of foods pre seeded
+        private static readonly List<Food> _foods = new()
+        {
+            new Food(1, "Applesauce", "mhmhmhm saucy sauce", 135),
+            new Food(2, "Polish Sausage", "Best found at central fresh meat market", 199),
+            new Food(3, "Roastes almonds", "People eat these?", 40)
+        };
+
+        // GET: /api/food
+        [HttpGet]
+        public ActionResult<List<Food>> GetAll()
+        {
+            return Ok(_foods);
+        }
+
+        // GET: /api/food/{id}
+        [HttpGet("{id:int}")]
+        public ActionResult<Food> GetById(int id)
+        {
+            var item = _foods.Find(f => f.Id == id);
+            if (item is null)
+            {
+                return NotFound(new { Message = $"Food with id '{id}' not found" });
+            }
+
+            return Ok(item);
+        }
+    }
+}
